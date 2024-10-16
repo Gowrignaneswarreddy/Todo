37be650bd16c851261f45dee2aff4f1b76b287f2
Gowrignaneswarreddy <gowrignaneswarreddy@gmail.com>
2024-10-16 19:23:12 +0530

    todo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoApp from './pages/todo'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <TodoApp/>
+    </div>
+  )
+}
+
+export default App
